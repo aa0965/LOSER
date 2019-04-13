5223ccf03df7000bf49b0a705d753532ba1e0838
aa0965 <anshupratapsingh32391@gmail.com>
2019-04-14 02:50:56 +0530

    'firstommit'

---
+++ index.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+
+app.set('views',__dirname + '/views');
+app.use(express.static(__dirname + '/JS'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+    var sql = require("mysql");
+
+    // config for your database
+    var connect = sql.createConnection({
+        user: 'sql',
+        password: '',
+        host: 'localhost',
+        database: 'PatientDB'
+    });
+
+    // connect to your database
+    connect.connect(function (err) {
+
+        if (err) console.log(err);
+      else console.log('connected');
+            });
+        app.get('/', function (req, res) {
+                      res.render('home.html');
+
+});
+
+
+
+app.post('/myaction', function(req, res) {
+  console.log(req.body.input[0]);
+ let bloodtype = req.body.input[0];
+
+    connect.query("SELECT * FROM PatientDB WHERE BloodType = "+sql.escape(bloodtype), function (err, result, fields) {
+      if (err) throw err;
+      res.render('gotit.ejs',{result:result});
+      console.log(result.length)
+    });
+
+});
+
+
+
+
+
+
+       app.listen(5000);
